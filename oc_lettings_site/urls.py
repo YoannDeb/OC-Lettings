19c281fc3e4d23e6c49cb519bde41b32e3ea7a3e
Yoann <yoann.deblois@gmail.com>
2022-03-22 17:00:46 +0100

    Refactor: Separation in two apps: lettings containing letting and addresses and profiles containing profile.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
+from lettings import urls as lettings_urls
+from profiles import urls as profiles_urls
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('lettings/', include(lettings_urls)),
+    path('profiles/', include(profiles_urls)),
     path('admin/', admin.site.urls),
 ]
