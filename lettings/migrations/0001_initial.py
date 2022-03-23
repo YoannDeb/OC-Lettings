1141cec4e8354e9151f5bf8b4caef1f806138c2f
Yoann <yoann.deblois@gmail.com>
2022-03-23 23:29:06 +0100

    bugfix: lettings transformed in templates by pycharm unwantedly.

--- lettings/migrations/0001_initial.py
+++ lettings/migrations/0001_initial.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=256)),
-                ('address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='templates.Address')),
+                ('address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='lettings.Address')),
             ],
         ),
     ]
