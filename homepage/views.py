9fe7e3719284c32ddb9bc3e804ed9bc217104a35
Yoann <yoann.deblois@gmail.com>
2022-03-24 01:30:57 +0100

    refactor: Migration of homepage view and url in a homepage app.

---
+++ homepage/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam
+# lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget
+# feugiat sagittis, sem mi convallis eros, vitae dapibus nisi lorem dapibus sem.
+# Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus
+# eleifend ex auctor venenatis tempus. Aliquam vitae erat ac orci placerat
+# luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in.
+# Praesent volutpat porttitor magna, non finibus neque cursus id.
+def index(request):
+    return render(request, 'homepage/index.html')
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 from . import views
 from lettings import urls as lettings_urls
 from profiles import urls as profiles_urls
+from homepage import urls as homepage_urls
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(homepage_urls, namespace='homepage')),
     path('lettings/', include(lettings_urls, namespace='lettings')),
     path('profiles/', include(profiles_urls, namespace='profiles')),
     path('admin/', admin.site.urls),
