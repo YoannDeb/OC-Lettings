9e9172555f07aa41fef2b48c1e50a3cc8569c629
Yoann <yoann.deblois@gmail.com>
2022-03-28 23:26:11 +0200

    func: tests for homepage app

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+client = Client()
+url = reverse('homepage:index')
+response = client.get(url)
+
+
+def test_homepage_should_respond_status_200_ok():
+    assert response.status_code == 200
+
+
+def test_homepage_response_should_return_title_in_html():
+    assert "<title>Holiday Homes</title>" in response.content.decode()
