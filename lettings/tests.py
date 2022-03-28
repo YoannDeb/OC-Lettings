a143c5835c9d20ef91a9dac9eb9871e9a34cf5e7
Yoann <yoann.deblois@gmail.com>
2022-03-29 00:44:28 +0200

    func: tests for lettings and profiles views

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,30 @@
-# from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import Letting, Address
 
-# Create your tests here.
+
+class LettingsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.address = Address.objects.create(number=1, street="rue de Paris", city="TestCity", state="", zip_code="75012", country_iso_code="FRA")
+        cls.letting = Letting.objects.create(title="Test", address=cls.address)
+
+    def test_lettings_index_should_respond_status_200_ok(self):
+        client = Client()
+        response = client.get(reverse('lettings:index'))
+        assert response.status_code == 200
+
+    def test_lettings_index_response_should_return_title_in_html(self):
+        client = Client()
+        response = client.get(reverse('lettings:index'))
+        assert "<title>Lettings</title>" in response.content.decode()
+
+    def test_lettings_letting_should_respond_status_200_ok(self):
+        client = Client()
+        response = client.get(reverse('lettings:letting', args=[1]))
+        assert response.status_code == 200
+
+    def test_lettings_letting_response_should_return_title_in_html(self):
+        client = Client()
+        response = client.get(reverse('lettings:letting', args=[1]))
+        assert "<title>Test</title>" in response.content.decode()
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,31 @@
-# from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class LettingsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username="test_username", password="abc1234", email="test@test.com", first_name="test", last_name='test')
+        cls.profile = Profile.objects.create(user=cls.user, favorite_city="Madrid")
+
+    def test_lettings_index_should_respond_status_200_ok(self):
+        client = Client()
+        response = client.get(reverse('profiles:index'))
+        assert response.status_code == 200
+
+    def test_lettings_index_response_should_return_title_in_html(self):
+        client = Client()
+        response = client.get(reverse('profiles:index'))
+        assert "<title>Profiles</title>" in response.content.decode()
+
+    def test_lettings_letting_should_respond_status_200_ok(self):
+        client = Client()
+        response = client.get(reverse('profiles:profile', args=["test_username"]))
+        assert response.status_code == 200
+
+    def test_lettings_letting_response_should_return_title_in_html(self):
+        client = Client()
+        response = client.get(reverse('profiles:profile', args=["test_username"]))
+        assert "<title>test_username</title>" in response.content.decode()
