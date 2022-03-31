d23df8ea526bbbeeec5c52165c55226aa25733be
Yoann <yoann.deblois@gmail.com>
2022-03-31 15:49:42 +0200

    func: Tests for homepage refactored as SimpleTestCase children.

--- tests/tests.py
+++ tests/tests.py
@@ -1,15 +1,17 @@
-from django.test import Client
+from django.test import Client, SimpleTestCase
 from django.urls import reverse
 
-client = Client()
-url = reverse('homepage:index')
 
+class HomepageSimpleTestCase(SimpleTestCase):
 
-def test_homepage_should_respond_status_200_ok():
-    response = client.get(url)
-    assert response.status_code == 200
+    def test_homepage_should_respond_status_200_ok(self):
+        client = Client()
+        url = reverse('homepage:index')
+        response = client.get(url)
+        assert response.status_code == 200
 
-
-def test_homepage_response_should_return_title_in_html():
-    response = client.get(url)
-    assert "<title>Holiday Homes</title>" in response.content.decode()
+    def test_homepage_response_should_return_title_in_html(self):
+        client = Client()
+        url = reverse('homepage:index')
+        response = client.get(url)
+        assert "<title>Holiday Homes</title>" in response.content.decode()
