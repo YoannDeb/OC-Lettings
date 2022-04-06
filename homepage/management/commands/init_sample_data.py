3c3fe22d8e9112269a2c4d946c5207330ee4fe03
Yoann <yoann.deblois@gmail.com>
2022-04-06 19:18:03 +0200

    style: conforming to linting rules.

--- homepage/management/commands/init_sample_data.py
+++ homepage/management/commands/init_sample_data.py
@@ -7,7 +7,8 @@ from profiles.models import Profile
 def create_address_and_associated_letting(number, street, city, state, zip_code,
                                           country_iso_code, letting_title):
     if not Address.objects.filter(
-            number=number, street=street, city=city, state=state, zip_code=zip_code, country_iso_code=country_iso_code
+            number=number, street=street, city=city, state=state,
+            zip_code=zip_code, country_iso_code=country_iso_code
     ).exists():
         address = Address.objects.create(
             number=number, street=street, city=city, state=state, zip_code=zip_code,
@@ -15,8 +16,10 @@ def create_address_and_associated_letting(number, street, city, state, zip_code,
         )
         address.save()
     else:
-        address = Address.objects.get(number=number, street=street, city=city, state=state, zip_code=zip_code,
-                                      country_iso_code=country_iso_code)
+        address = Address.objects.get(
+            number=number, street=street, city=city, state=state, zip_code=zip_code,
+            country_iso_code=country_iso_code
+        )
 
     if not Letting.objects.filter(title=letting_title, address=address).exists():
         letting = Letting.objects.create(title=letting_title, address=address)
@@ -48,21 +51,31 @@ class Command(BaseCommand):
             admin_user.is_staff = True
             admin_user.save()
 
-        create_address_and_associated_letting(7217, "Bedford Street", "Brunswick", "GA", 31525, "USA",
+        create_address_and_associated_letting(7217, "Bedford Street", "Brunswick",
+                                              "GA", 31525, "USA",
                                               "Joshua Tree Green Haus /w Hot Tub")
-        create_address_and_associated_letting(4, "Military Street", "Willoughby", "OH", 44094, "USA",
+        create_address_and_associated_letting(4, "Military Street", "Willoughby",
+                                              "OH", 44094, "USA",
                                               "Oceanview Retreat")
-        create_address_and_associated_letting(340, "Wintergreen Avenue", "Newport News", "VA", 23601, "USA",
+        create_address_and_associated_letting(340, "Wintergreen Avenue", "Newport News",
+                                              "VA", 23601, "USA",
                                               "'Silo Studio' Cottage")
-        create_address_and_associated_letting(9230, "E. Joy Ridge Street", "Marquette", "MI", 49855, "USA",
+        create_address_and_associated_letting(9230, "E. Joy Ridge Street", "Marquette",
+                                              "MI", 49855, "USA",
                                               "Pirates of the Caribbean Getaway")
-        create_address_and_associated_letting(9606, "Harvard Street", "Aliquippa", "PA", 15001, "USA",
-                                              "The Mushroom Dome Retreat & LAND of Paradise Suite")
-        create_address_and_associated_letting(588, "Argyle Avenue", "East Meadow", "NY", 11554, "USA",
-                                              "Underground Hygge")
+        create_address_and_associated_letting(
+            9606, "Harvard Street", "Aliquippa", "PA",
+            15001, "USA", "The Mushroom Dome Retreat & LAND of Paradise Suite"
+        )
+        create_address_and_associated_letting(588, "Argyle Avenue", "East Meadow",
+                                              "NY", 11554, "USA", "Underground Hygge")
 
-        create_user_and_associated_profile("HeadlinesGazer", "jssssss33@acee9.live", "Jamie", "Lal", "Buenos Aires")
-        create_user_and_associated_profile("DavWin", "5houssam.kessaiso@facpidif.ml", "Grahm", "Cassandra", "Barcelona")
-        create_user_and_associated_profile("AirWow", "flocation.vam4@glendenningflowerdesign.com", "Ada", "Paul",
-                                           "Budapest")
-        create_user_and_associated_profile("4meRomance", "coemperor@famemma.net", "John", "Rodriguez", "Berlin")
+        create_user_and_associated_profile("HeadlinesGazer", "jssssss33@acee9.live",
+                                           "Jamie", "Lal", "Buenos Aires")
+        create_user_and_associated_profile("DavWin", "5houssam.kessaiso@facpidif.ml",
+                                           "Grahm", "Cassandra", "Barcelona")
+        create_user_and_associated_profile("AirWow",
+                                           "flocation.vam4@glendenningflowerdesign.com",
+                                           "Ada", "Paul", "Budapest")
+        create_user_and_associated_profile("4meRomance", "coemperor@famemma.net",
+                                           "John", "Rodriguez", "Berlin")
