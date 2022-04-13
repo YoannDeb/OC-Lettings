33c0a5e2defae003c18480171f7bbed1789ae518
Yoann <yoann.deblois@gmail.com>
2022-04-13 20:45:22 +0200

    Docs: Docstrings.

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,11 @@ from .models import Profile
 # consequat libero pulvinar eget. Fusc faucibus, urna quis auctor pharetra,
 # massa dolor cursus neque, quis dictum lacus d
 def index(request):
+    """
+    Profiles index view, which shows a listing of all profiles.
+    :param request: the request sent by the client.
+    :return: A render of the profiles' index.html page.
+    """
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
     return render(request, 'profiles/index.html', context)
@@ -18,6 +23,11 @@ def index(request):
 # dignissim congue. Pellentesque habitant morbi tristique senectus et netus et
 # males
 def profile(request, username):
+    """
+    Profiles profiles view, which shows details about one profile.
+    :param request: the request sent by the client.
+    :return: A render of the profiles' profile.html page.
+    """
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
     return render(request, 'profiles/profile.html', context)
