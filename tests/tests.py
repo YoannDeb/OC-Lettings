9e3fccf2e375ee5c4c96236e8142212c412076a8
Yoann <yoann.deblois@gmail.com>
2022-04-13 20:44:46 +0200

    Docs: Docstrings.

--- tests/tests.py
+++ tests/tests.py
@@ -5,12 +5,20 @@ from django.urls import reverse
 class HomepageSimpleTestCase(SimpleTestCase):
 
     def test_homepage_should_respond_status_200_ok(self):
+        """
+        Testing that GET request on homepage index page is returning status code 200,
+        which means ok.
+        """
         client = Client()
         url = reverse('homepage:index')
         response = client.get(url)
         assert response.status_code == 200
 
     def test_homepage_response_should_return_title_in_html(self):
+        """
+        Testing that GET request on homepage index page returns html with correct
+        title inside.
+        """
         client = Client()
         url = reverse('homepage:index')
         response = client.get(url)
